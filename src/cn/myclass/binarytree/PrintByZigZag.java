6a686b82b49105209abcdef9f293dc30db354e0f
guanxiaodong <kimehwa@qq.com>
2019-07-08 11:30:40 +0800

    5/16

---
+++ src/cn/myclass/binarytree/PrintByZigZag.java
@@ -0,0 +1,21 @@
+package cn.myclass.binarytree;
+
+public class PrintByZigZag {
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+
+            this.value = data;
+        }
+    }
+
+    public void printByZigZag(Node head) {
+        if (head == null) {
+            return;
+        }
+
+    }
+}
